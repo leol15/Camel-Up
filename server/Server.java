15c9627110e11d3218782fb47504ed785bdee4de
leo-u <leol15@uw.edu>
2020-11-28 13:47:59 -0800

    add testing routes

--- server/Server.java
+++ server/Server.java
@@ -13,81 +13,93 @@ import com.google.gson.*;
 public class Server {
 
 	static final int PORT = 45678;
+	static final String GAME_ROUTE = "/game";
 
-    public static void main(String[] args) {
-        port(PORT);
-        ///////////////////////
-        // CORSFILTER stuff  //
-        ///////////////////////
+	public static void main(String[] args) {
+		port(PORT);
+		///////////////////////
+		// CORSFILTER stuff  //
+		///////////////////////
 
-        final HashMap<String, String> corsHeaders = new HashMap<>();
-        corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-        corsHeaders.put("Access-Control-Allow-Origin", "*");
-        corsHeaders.put("Access-Control-Allow-Headers",
-                "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
-        corsHeaders.put("Access-Control-Allow-Credentials", "true");
-        Filter filter = new Filter() {
-            @Override
-            public void handle(Request request, Response response) {
-                corsHeaders.forEach(response::header);
-                System.out.print("___Client IP: ");
-                System.out.print(request.ip());
-                System.out.print(" -> ");
-                System.out.println(request.url());
-            }
-        };
-        Spark.afterAfter(filter); // Applies this filter even if there's a halt() or exception.
-        ////////////////////////
-        // End of CORSFILTER  //
-        ////////////////////////
-        /////////////////
-        // initialize  //
-        /////////////////
+		final HashMap<String, String> corsHeaders = new HashMap<>();
+		corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+		corsHeaders.put("Access-Control-Allow-Origin", "*");
+		corsHeaders.put("Access-Control-Allow-Headers",
+				"Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
+		corsHeaders.put("Access-Control-Allow-Credentials", "true");
+		Filter filter = new Filter() {
+			@Override
+			public void handle(Request request, Response response) {
+				corsHeaders.forEach(response::header);
+				System.out.print("___Client IP: ");
+				System.out.print(request.ip());
+				System.out.print(" -> ");
+				System.out.println(request.url());
+			}
+		};
+		Spark.afterAfter(filter); // Applies this filter even if there's a halt() or exception.
+		////////////////////////
+		// End of CORSFILTER  //
+		////////////////////////
 
-        CamelUp game = new CamelUp();
-        
+		/////////////////
+		// initialize  //
+		/////////////////
+
+		HashMap<String, CamelUp> gamePool = new HashMap<>();
+		Gson gson = new Gson();
 
 
-        Gson gson = new Gson();
-        // default
-        //Spark.get("/", )
-        get("/", (req, res) -> {
-            try {
 
-                // String name = req.queryParams("name");
-                // String message = req.queryParams("message");
-    //            Map<String, String[]> mp = req.queryMap().toMap();
-            } catch (Exception e) {
-                System.out.println(e);
-                e.printStackTrace();
-                return "bad";
-            }
-            return "";
-        });
 
-        get("/chat", (req, res) -> {
-//            res.body(gson.toJson("ok"));
-//            res.type("test/json");
-            // Update chat
-//            updateChat(history, DATE.getTime());
-            // Return chat history
-            // return gson.toJson(history);
-            return "";
-        	
-        });
+		// return a new game link to use
+		post("/startgame", (req, res) -> {
+			Random r = new Random();
+			while (true) {
+				String roomLink = "";
+				for (int i = 0; i < 5; i++) {
+					roomLink += (char) ('a' + r.nextInt(26));
+				}
+				if (gamePool.containsKey(roomLink))
+					continue;
+				// create game
+				gamePool.put(roomLink, new CamelUp());
+				return roomLink;
+			}
 
-        get("/clear", (req, res) -> {
-//            res.body(gson.toJson("ok"));
-//            res.type("test/json");
-            // Update chat
-            // Return chat history
-            return "";
-        });
+		});
 
-        get("/page", (req, res) -> {
-            res.body(gson.toJson("ok"));
-            res.type("test/json");
-            return "";
-        });
+		get(GAME_ROUTE + "/*", (req, res) -> {
+			if (req.splat().length == 0)
+				return "";
+			// connect request to specific room
+			String roomLink = req.splat()[0];
+			if (!gamePool.containsKey(roomLink))
+				return "";
+			CamelUp game = gamePool.get(roomLink);
+			if (game == null)
+				return "";
+			// or return something like room does not exist
+			// return game info, todo
+			return game.toString();
+		});
+
+		// default
+		// should we serve the web as well?
+		get("/", (req, res) -> {
+			try {
+				// read a local html file
+				// and send it back
+				// String name = req.queryParams("name");
+				// Map<String, String[]> mp = req.queryMap().toMap();
+				return "";
+			} catch (Exception e) {
+				System.out.println(e);
+				e.printStackTrace();
+				return "bad";
+			}
+		});
+
 	}
 }
+
